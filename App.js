2f7d2befcaba1dbdb894fee4e19b9c14721b6597
Alvar Aronija <alvar@weappdevelopers.com>
2020-02-14 21:52:03 +0100

    First commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello from CircleCI!</Text>
+        <Text>Hello from Sweden!</Text>
       </View>
     );
   }
